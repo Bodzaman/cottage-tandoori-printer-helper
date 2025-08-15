5addacdc2e90fee24fba065dde96ba2196fe4c52
Bod <164583370+Bodzaman@users.noreply.github.com>
2025-08-15 23:44:18 +0100

    Add service-uninstall.js for Windows Service

--- service-uninstall.js
+++ service-uninstall.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 // Create a new service object
 const svc = new Service({
-    name: 'Cottage Tandoori Printer Helper',
+    name: 'PrinterHelperService',
     script: path.join(__dirname, 'server.js')
 });
 
 // Listen for the "uninstall" event so we know when it's gone.
 svc.on('uninstall', function() {
-    console.log('Cottage Tandoori Printer Helper uninstalled successfully');
+    console.log('PrinterHelperService uninstalled successfully');
 });
 
 // Uninstall the service.
-console.log('Uninstalling Cottage Tandoori Printer Helper...');
+console.log('Uninstalling PrinterHelperService...');
 svc.uninstall();
